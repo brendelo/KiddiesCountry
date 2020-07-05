42a0006d6db7352ba64de8586f2fd1f401f48b46
BrendanOdigwe <brendelo@yahoo.co.uk>
2020-07-05 12:02:14 -0400

    fix the subheader issue in all header links except HOME

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -214,6 +214,19 @@
 
 @endsection
 
+@section('subheader')
+@section('subheader')
+    <div id="Subheader" style="padding:130px 0 230px">
+        <div class="container">
+            <div class="column one">
+                <h1 class="title">About Us</h1>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@endsection
+
 
 
 
