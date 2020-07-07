f90daf6810fa281138cde77514658e6dcdd662c7
BrendanOdigwe <brendelo@yahoo.co.uk>
2020-07-06 22:32:30 -0400

    create admin directory with admin page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index(){
-        return view('home');
+        return view('index');
     }
 
-    public function Admin(){
+    public function admin(){
         return view('welcome');
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,16 +68,19 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/') }}">Home</a>
+                        <a href="{{ url('/') }}">Kiddies</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
                         @endif
+                        <a href="{{ url('/') }}">Kiddies</a>
                     @endauth
                 </div>
             @endif
+
 
             <div class="content">
                 <div class="title m-b-md">
